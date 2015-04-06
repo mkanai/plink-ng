557f418406b08765e6e0da5410eaa54a2ea10b2b
Masahiro Kanai <mkanai.brc@tmd.ac.jp>
2015-04-06 17:26:24 +0900

    resolve --filter-* + --thin-indiv-count bug

--- plink.c
+++ plink.c
@@ -299,7 +299,9 @@ int32_t plink(char* outname, char* outname_end, char* bedname, char* bimname, ch
   uintptr_t unfiltered_sample_ct4 = 0;
   uintptr_t unfiltered_sample_ctl = 0;
   uintptr_t* sample_exclude = NULL;
+  uintptr_t* sample_exclude_new = NULL;
   uintptr_t sample_exclude_ct = 0;
+  uintptr_t sample_exclude_ct_new = 0;
   uintptr_t sample_ct = 0;
   uint32_t* sample_sort_map = NULL;
   uintptr_t* founder_info = NULL;
@@ -978,7 +980,6 @@ int32_t plink(char* outname, char* outname_end, char* bedname, char* bimname, ch
     if (om_ip->marker_fname) {
       // would rather do this with pre-sorted markers, but that might break
       // order-of-operations assumptions in existing pipelines
-      retval = load_oblig_missing(bedfile, bed_offset, unfiltered_marker_ct, marker_exclude, marker_exclude_ct, marker_ids, max_marker_id_len, cptr, ulii, max_sample_id_len, uiptr, unfiltered_sample_ct, sample_exclude, sex_male, chrom_info_ptr, om_ip);
       if (retval) {
 	goto plink_ret_1;
       }
@@ -1067,7 +1068,7 @@ int32_t plink(char* outname, char* outname_end, char* bedname, char* bimname, ch
       if (random_thin_samples(thin_keep_sample_prob, unfiltered_sample_ct, sample_exclude, &sample_exclude_ct)) {
         goto plink_ret_ALL_SAMPLES_EXCLUDED;
       }
-    } else if (thin_keep_sample_ct & !(glm_modifier & GLM_RANDOMIZATION)) {
+    } else if (thin_keep_sample_ct && !(glm_modifier & GLM_RANDOMIZATION)) {
       retval = random_thin_samples_ct(thin_keep_sample_ct, unfiltered_sample_ct, sample_exclude, &sample_exclude_ct);
       if (retval) {
         goto plink_ret_1;
@@ -1951,6 +1952,12 @@ int32_t plink(char* outname, char* outname_end, char* bedname, char* bimname, ch
               pheno_nm_ct = unfiltered_sample_ct;
               for (ulii = 0; ulii < unfiltered_sample_ct; ulii++) {set_bit(pheno_nm, ulii);}
             }
+
+            sample_exclude_ct_new = sample_exclude_ct;
+            if (wkspace_alloc_ul_checked(&sample_exclude_new, unfiltered_sample_ctl * sizeof(intptr_t))) {
+              goto plink_ret_NOMEM;
+            }
+            memcpy(sample_exclude_new, sample_exclude, unfiltered_sample_ctl * sizeof(intptr_t));
         }
 
         for (; uii <= perm_end; uii++) {
@@ -1961,23 +1968,23 @@ int32_t plink(char* outname, char* outname_end, char* bedname, char* bimname, ch
             sfmt_init_gen_rand(&sfmt, uii);
             if (thin_keep_sample_ct) {
               if (sample_exclude_ct) {
-                clear_bits(sample_exclude, 0, unfiltered_sample_ct);
-                sample_exclude_ct = 0;
+                memcpy(sample_exclude_new, sample_exclude, unfiltered_sample_ctl * sizeof(intptr_t));
+                sample_exclude_ct_new = sample_exclude_ct;
               }
-              retval = random_thin_samples_ct(thin_keep_sample_ct, unfiltered_sample_ct, sample_exclude, &sample_exclude_ct);
+              retval = random_thin_samples_ct(thin_keep_sample_ct, unfiltered_sample_ct, sample_exclude_new, &sample_exclude_ct_new);
               if (retval) {
                 goto plink_ret_1;
               }
-              sample_ct = unfiltered_sample_ct - sample_exclude_ct;
+              sample_ct = unfiltered_sample_ct - sample_exclude_ct_new;
               sfmt_init_gen_rand(&sfmt, uii);
             }
             for (ujj = 0, uljj = 0; uljj < sample_ct; ujj++, uljj++) {
-              next_unset_unsafe_ck(sample_exclude, &ujj);
+              next_unset_unsafe_ck(sample_exclude_new, &ujj);
               if (sfmt_genrand_uint32(&sfmt) % 2) {set_bit(pheno_c, ujj);}
               else {clear_bit(pheno_c, ujj);}
             }
           }
-	      retval = glm_logistic_assoc(threads, bedfile, bed_offset, outname, outname_end2, glm_modifier, glm_vif_thresh, glm_xchr_model, glm_mperm_val, parameters_range_list_ptr, tests_range_list_ptr, ci_size, ci_zt, pfilter, output_min_p, mtest_adjust, adjust_lambda, unfiltered_marker_ct, marker_exclude, marker_ct, marker_ids, max_marker_id_len, plink_maxsnp, marker_pos, marker_allele_ptrs, max_marker_allele_len, marker_reverse, condition_mname, condition_fname, chrom_info_ptr, unfiltered_sample_ct, sample_ct, sample_exclude, cluster_ct, cluster_map, cluster_starts, apip, mperm_save, pheno_nm_ct, pheno_nm, pheno_c, covar_ct, covar_names, max_covar_name_len, covar_nm, covar_d, founder_info, sex_nm, sex_male, ldip->modifier & LD_IGNORE_X, hh_exists, perm_batch_size, sip);
+	      retval = glm_logistic_assoc(threads, bedfile, bed_offset, outname, outname_end2, glm_modifier, glm_vif_thresh, glm_xchr_model, glm_mperm_val, parameters_range_list_ptr, tests_range_list_ptr, ci_size, ci_zt, pfilter, output_min_p, mtest_adjust, adjust_lambda, unfiltered_marker_ct, marker_exclude, marker_ct, marker_ids, max_marker_id_len, plink_maxsnp, marker_pos, marker_allele_ptrs, max_marker_allele_len, marker_reverse, condition_mname, condition_fname, chrom_info_ptr, unfiltered_sample_ct, sample_ct, sample_exclude_new, cluster_ct, cluster_map, cluster_starts, apip, mperm_save, pheno_nm_ct, pheno_nm, pheno_c, covar_ct, covar_names, max_covar_name_len, covar_nm, covar_d, founder_info, sex_nm, sex_male, ldip->modifier & LD_IGNORE_X, hh_exists, perm_batch_size, sip);
         }
 	  }
 	} else {
